9a36a2a50310fcb2d3ab47775971ae5f0f9f16e9
lsc <695788857@qq.com>
2022-03-28 23:22:06 +0800

    Aspect9

---
+++ src/main/java/com/lsc/test/aspect/Aspect9.java
@@ -0,0 +1,25 @@
+package com.lsc.test.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
+
+@org.springframework.stereotype.Service
+@org.aspectj.lang.annotation.Aspect
+public class Aspect9 {
+
+    @Pointcut("execution(* com.lsc.test.service..*.*(..))")
+    public void pointCut(){}
+
+    @Around("pointCut()")
+    public Object handleServiceMethod(ProceedingJoinPoint pjp){
+        Object data ;
+        try {
+            data = pjp.proceed();
+            return data;
+        } catch (Throwable throwable) {
+            return null;
+        }
+    }
+
+}
