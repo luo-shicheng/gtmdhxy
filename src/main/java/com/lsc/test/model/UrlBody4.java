93b97779765301de71e717e272a9a90d201fe2e0
lsc <695788857@qq.com>
2022-03-31 22:46:51 +0800

    UrlBody4

---
+++ src/main/java/com/lsc/test/model/UrlBody4.java
@@ -0,0 +1,28 @@
+package com.lsc.test.model;
+
+import javax.validation.constraints.NotBlank;
+
+public class UrlBody4 {
+
+    @NotBlank(message = "id不能为空")
+    private String id;
+
+    @NotBlank(message = "id1不能为空")
+    private String id1;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId1() {
+        return id1;
+    }
+
+    public void setId1(String id1) {
+        this.id1 = id1;
+    }
+}
